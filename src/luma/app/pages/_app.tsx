b8ee81948d9d830d6f867eeb2856abf2d30d9379
Balaji Veeramani <bveeramani@berkeley.edu>
2025-12-14 17:45:08 -0800

    Initial commit

--- src/luma/app/pages/_app.tsx
+++ src/luma/app/pages/_app.tsx
@@ -9,18 +9,12 @@ import "prismjs";
 // Import other Prism themes here
 import "prismjs/components/prism-bash.min";
 import "prismjs/components/prism-python.min";
+import "../lib/prism-python-repl"; // Extends Python to tokenize REPL prompts
 import "prismjs/components/prism-yaml.min";
 import "prismjs/components/prism-markdown.min";
 import "../styles/prism-theme.css";
-import { useRouter } from "next/router";
-
-import "prismjs";
-import "prismjs/components/prism-bash.min";
-import "prismjs/components/prism-python.min";
-import "prismjs/components/prism-yaml.min";
-
-import "../styles/prism-theme.css";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
 
 import type { AppProps } from "next/app";
 import type { MarkdocNextJsPageProps } from "@markdoc/next.js";
