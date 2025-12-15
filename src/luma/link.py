fb880e34bc97fb25a3a8de98cd7aec51480d7e2a
Nick Spevacek <42660771+nspevacek@users.noreply.github.com>
2025-12-14 17:29:23 -0800

    Add image support (#48)
    * Add image support
    
    * Skip gitignored files

--- src/luma/link.py
+++ src/luma/link.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from pathlib import Path
+from pathspec import PathSpec
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -9,18 +10,8 @@ from watchdog.observers import Observer
 from .config import ResolvedConfig
 from .node import get_node_root
 
+STATIC_EXTENSIONS = (".jpg", ".jpeg", ".png", ".webp", ".gif", ".ico")
 logger = logging.getLogger(__name__)
-
-
-def link_static_asset(relative_path: str, project_root: str):
-    src = os.path.join(project_root, relative_path)
-    dst = os.path.join(get_node_root(project_root), "public", relative_path)
-
-    if os.path.exists(dst):
-        os.remove(dst)
-
-    logger.debug(f"Linking file from '{src}' to '{dst}'")
-    os.link(src, dst)
 
 
 def link_config(resolved_config: ResolvedConfig, project_root: str):
@@ -54,6 +45,45 @@ def _link_page(project_root: str, relative_path: str):
 
     logger.debug(f"Linking page from '{src}' to '{dst}'")
     os.makedirs(os.path.dirname(dst), exist_ok=True)
+    os.link(src, dst)
+
+
+def link_static_assets(project_root: str):
+    ignore_spec = _load_ignore_spec(project_root)
+
+    for dir_path, _, filenames in os.walk(project_root):
+        for filename in filenames:
+            if not filename.endswith(STATIC_EXTENSIONS):
+                continue
+
+            file_path = os.path.join(dir_path, filename)
+            relative_path = os.path.relpath(file_path, project_root)
+            if relative_path.startswith(".luma"):
+                continue
+
+            if not ignore_spec or not ignore_spec.match_file(relative_path):
+                _link_static_asset(project_root, relative_path)
+
+
+def _load_ignore_spec(project_root: str):
+    ignore_path = os.path.join(project_root, ".gitignore")
+
+    if not os.path.exists(ignore_path):
+        logger.debug("No .gitignore found")
+        return None
+
+    with open(ignore_path, "r") as file:
+        return PathSpec.from_lines("gitwildmatch", file)
+
+
+def _link_static_asset(project_root: str, relative_path: str):
+    src = os.path.join(project_root, relative_path)
+    dst = os.path.join(get_node_root(project_root), "public", relative_path)
+
+    if os.path.exists(dst):
+        os.remove(dst)
+
+    logger.debug(f"Linking file from '{src}' to '{dst}'")
     os.link(src, dst)
 
 
